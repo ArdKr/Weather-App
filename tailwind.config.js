9c580d229be80d3bc5df7171ce103cfb265051a1
ArdKr <mkras01@gmx.de>
2020-12-30 19:34:01 +0100

    Configure TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          light: "#3C47E9",
+          DEFAULT: "#1E213A",
+          dark: "#100E1D",
+        },
+        gray: {
+          DEFAULT: "#E7E7EB",
+          dark: "#6E707A",
+        },
+      },
+      fontFamily: {
+        sans: ["Raleway", "sans-serif"],
+      },
+      fontSize: {
+        normal: "1.6rem",
+        md: "1.8rem",
+        lg: "2.4rem",
+        "2xl": "3.6rem",
+        "3xl": "4.8rem",
+        "4xl": "6rem",
+      },
+    },
   },
   variants: {
     extend: {},
