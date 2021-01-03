c6adad797ad6a367a83efb74134fab06eeedac15
ArdKr <mkras01@gmx.de>
2021-01-03 14:40:24 +0100

    Add Loading handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 
-import WeekWeatherCards from "./components/WeekWeatherCards";
+import MainSide from "./components/MainSide";
+
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const weatherData = useSelector((state) => state.weather.data);
+
+  if (weatherData.length === 0) {
+    return (
+      <div className="mx-auto w-64 h-64 mt-80">
+        <div >
+          <img src="/loading.svg" alt="loading svg"/>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="w-screen">
       <p>!</p>
 
-      <WeekWeatherCards />
+      <MainSide />
     </div>
   );
 };
---
+++ src/components/MainSide/MainSide.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import WeekWeatherCards from "../WeekWeatherCards";
+
+const MainSide = () => {
+  return (
+    <div className="container mx-auto py-16 px-20 md:px-0">
+      <WeekWeatherCards />
+    </div>
+  );
+};
+
+export default MainSide;
