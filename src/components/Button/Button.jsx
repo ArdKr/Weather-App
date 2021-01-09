1003edd3330b53b976fdefe6ddcd265bc8ad7205
ArdKr <mkras01@gmx.de>
2021-01-09 19:34:58 +0100

    Fix PurgeCSS bug

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+// The button below is screwed (I was running into some PurgeCSS problems). I need to refactor it later when I have time.
+
 const Button = ({
   children,
   fullHeight = false,
@@ -9,32 +11,49 @@ const Button = ({
   rounded = false,
   paddingEqual = false,
 }) => {
-  const styleClasses = () => {
-    let classes = `bg-${color} text-white text-normal cursor-pointer text-medium focus:outline-none`;
+  // const styleClasses = () => {
+  //   let classes = `bg-${color} text-white text-normal cursor-pointer text-medium focus:outline-none`;
 
-    if (border) {
-      classes += " border";
-    }
+  //   if (border) {
+  //     classes += " border";
+  //   }
 
-    if (fullHeight) {
-      classes += " h-full";
-    }
+  //   if (fullHeight) {
+  //     classes += " h-full";
+  //   }
 
-    if (rounded) {
-      classes += " rounded-full";
-    }
+  //   if (rounded) {
+  //     classes += " rounded-full";
+  //   }
 
-    if (paddingEqual) {
-      classes += " h-20 w-20";
-    } else {
-      classes += " py-4 px-8";
-    }
+  //   if (paddingEqual) {
+  //     classes += " h-20 w-20";
+  //   } else {
+  //     classes += " py-4 px-8";
+  //   }
 
-    return classes;
+  //   return classes;
+  // };
+
+  const classes = {
+    border: " border",
+    fullHeight: " h-full",
+    rounded: " rounded-full",
+    paddingEqual: " h-20 w-20",
+    paddingNotEqueal: " py-4 px-8",
   };
+
   return (
     <React.Fragment>
-      <button className={styleClasses()} onClick={onClick}>
+      {/* <button className={styleClasses()} onClick={onClick}> */}
+      <button
+        className={`bg-${color} text-white text-normal cursor-pointer text-medium focus:outline-none${
+          border ? classes.border : ""
+        }${fullHeight ? classes.fullHeight : ""}${
+          rounded ? classes.rounded : ""
+        }${paddingEqual ? classes.paddingEqual : classes.paddingNotEqueal}`}
+        onClick={onClick}
+      >
         {children}
       </button>
     </React.Fragment>
